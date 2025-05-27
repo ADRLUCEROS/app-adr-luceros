e706fd5eacab8fd231384227fe079b134812832a
luis-keny <luis.lucero03@outlook.com>
2025-05-27 10:54:44 -0500

    Add ROUTES constant for managing application navigation

---
+++ src/const/router.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  manage: {
+    store: '/manage-store',
+    truck: '/manage-truck',
+    worker: '/manage-worker',
+    partner: '/manage-partner',
+    delivery: '/manage-delivery',
+  }
+}
