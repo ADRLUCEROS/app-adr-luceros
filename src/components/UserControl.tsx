433b07d163b64393dd1f9819f3dd2da30ec37069
luis-keny <luis.lucero03@outlook.com>
2025-06-03 02:29:15 -0500

    Add UserControl component with dropdown menu and user avatar

---
+++ src/components/UserControl.tsx
@@ -0,0 +1,49 @@
+import * as React from "react"
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarImage } from "./ui/avatar"
+
+export function UserControl() {
+  const [open, setOpen] = React.useState(false)
+  return (
+    <div className="flex w-full flex-row items-start justify-between px-2 py-2">
+      <div className="text-sm leading-none font-medium flex items-center gap-2">
+        <Avatar>
+          <AvatarImage src="https://images.steamusercontent.com/ugc/851600242208157774/7FBB64059B18F161EFADFF3C0B41F906F3D4AE2B/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false" />
+        </Avatar>
+        <p className="flex flex-col text-left">
+          <span className="truncate">Moises Tapia</span>
+          <span className="truncate text-xs text-blue-300">Administrador</span>
+        </p>
+      </div>
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="sm">
+            <MoreHorizontal />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[200px]">
+          <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+          <DropdownMenuGroup>
+            <DropdownMenuItem>Configuración</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-red-600">
+              Cerrar sesión
+              <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
